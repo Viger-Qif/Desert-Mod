8a63cfc88324bc7002a356ec86a5ba5b9a60adc5
mxnder <lien.escapist@ya.ru>
2026-09-06 18:49:13 +0700

    результат 06.09 fix

--- src/client/java/net/mxnder/desertmod/client/datagen/ModModelProvider.java
+++ src/client/java/net/mxnder/desertmod/client/datagen/ModModelProvider.java
@@ -16,6 +16,7 @@ public class ModModelProvider extends FabricModelProvider {
 
     @Override
     public void generateBlockStateModels(BlockModelGenerators blockModelGenerators) {
+        blockModelGenerators.createTrivialCube(ModBlocks.KIFI_BRAZIER);
     }
 
     @Override
